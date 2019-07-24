acb6a655801b7c57d0c6ff7910200778ed6d5f9d
Pesach <pesach020@gmail.com>
2019-07-24 19:48:42 +0300

    Updates

---
+++ precache-manifest.4fbe84a7179de445e634b2e2371ef28c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c258b5540bb96fc8c538dfc1e26fe9",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "9e500543e8ad632332fd",
+    "url": "/QuizApp/static/css/main.7738e31e.chunk.css"
+  },
+  {
+    "revision": "91b86a4d6df9977993b3",
+    "url": "/QuizApp/static/js/2.2f5f1ca4.chunk.js"
+  },
+  {
+    "revision": "9e500543e8ad632332fd",
+    "url": "/QuizApp/static/js/main.2c9e537d.chunk.js"
+  },
+  {
+    "revision": "7d042752d8a6cfaf63b7",
+    "url": "/QuizApp/static/js/runtime~main.0a40b9c9.js"
+  },
+  {
+    "revision": "3d1d82077bd414e23582e714f937c144",
+    "url": "/QuizApp/static/media/family.3d1d8207.jpg"
+  },
+  {
+    "revision": "c39ee950b320e3147fa29e80430792c8",
+    "url": "/QuizApp/static/media/img1.c39ee950.jpg"
+  },
+  {
+    "revision": "b5a0408dff8c7215018158ab319c6508",
+    "url": "/QuizApp/static/media/img2.b5a0408d.jpg"
+  },
+  {
+    "revision": "acc6abc89ca3ccea4d2cd4362c8f732b",
+    "url": "/QuizApp/static/media/img3.acc6abc8.jpg"
+  },
+  {
+    "revision": "c9e37d2c4f2b6f4400e1f0ab74e0c6c0",
+    "url": "/QuizApp/static/media/img4.c9e37d2c.jpeg"
+  },
+  {
+    "revision": "6a10ac076f8c5ea08deb3307475682a6",
+    "url": "/QuizApp/static/media/img5.6a10ac07.jpg"
+  },
+  {
+    "revision": "b32af33f328d67db1f2ff519b1e76e5b",
+    "url": "/QuizApp/static/media/img6.b32af33f.jpg"
+  },
+  {
+    "revision": "751233b4a85a51673a458bae7d40e92e",
+    "url": "/QuizApp/static/media/img7.751233b4.png"
+  },
+  {
+    "revision": "64b88f2a32d3720468c610943ae8be22",
+    "url": "/QuizApp/static/media/img8.64b88f2a.jpg"
+  },
+  {
+    "revision": "7d912e4ded448e34ba628d61cab9a4cd",
+    "url": "/QuizApp/static/media/quiz.7d912e4d.png"
+  }
+]);
